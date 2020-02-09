8a2a582f8d7cc9166556f4ac8ea29f3841435fb6
Maxime Salomon <maxime@croissant.io>
2020-02-09 22:46:14 +0100

    add visitors Router + POST + GET by id endpoint outline

---
+++ backend/routes/visitorRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const router = express.Router();
+
+// GET visitor by id
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  res.status(200).send(`hello from the /GET /visitors/${id} endpoint.`);
+});
+
+//POST visitor
+router.post("/", (re, res) => {
+  res.status(200).send(`hello from the /POST /visitors endpoint.`);
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
 const express = require("express");
 
+const visitorRoutes = require("./routes/visitorRoutes");
+
 const server = express();
 const PORT = process.env.PORT || 7000;
+
+server.use("/visitors", visitorRoutes);
 
 // Server listening
 server.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
-
-// API Homepage
-server.get("/", (req, res) => {
-  res.send("<h1>Enrichment</h1>");
-});
