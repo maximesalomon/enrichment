de6e3cf3fbe623a9dccbb07bbf2c0c068603ae80
Maxime Salomon <maxime@croissant.io>
2020-02-09 21:55:43 +0100

    install react

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,24 @@
-import React from '../node_modules/@types/react';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () =>  {
+function App() {
   return (
     <div className="App">
-      <h1>Enrichment 🛠</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- Frontend/src/App.test.js
+++ Frontend/src/App.test.js
@@ -1,3 +1,9 @@
-import React from '../node_modules/@types/react';
-import { render } from '../node_modules/@types/testing-library__react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
