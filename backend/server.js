65eb4b2bfd75dc776dd2dff16c5b3e70c28485c0
Maxime Salomon <maxime@croissant.io>
2020-02-09 23:04:41 +0100

    api home comment

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ server.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
 
+// API Home
 server.use("/", (req, res) => {
   res.status(200).send(`<h1>Enrichment 🛠</h1>`);
 });
