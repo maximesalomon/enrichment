809b2cec05465e15f86307469d533019c696b8f3
Maxime Salomon <maxime@croissant.io>
2020-02-09 22:17:52 +0100

    setup express server

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,14 @@
-console.log("hello, world!")
+const express = require("express");
+
+const server = express();
+const PORT = process.env.PORT || 7000;
+
+// Server listening
+server.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
+
+// API Homepage
+server.get("/", (req, res) => {
+  res.send("<h1>Enrichment</h1>");
+});
