c98d4cd4ffad2fd0b58e2f3b66593051df1d3032
Maxime Salomon <maxime@croissant.io>
2020-02-10 09:04:07 +0100

    POST request

--- Backend/routes/requestRoutes.js
+++ Backend/routes/requestRoutes.js
@@ -2,35 +2,33 @@ const express = require("express");
 const uuidv4 = require("uuid/v4");
 const bodyParser = require("body-parser");
 
-const clearbit = require('clearbit')(CLEARBIT_SECRET_API_KEY);
+const clearbit = require("clearbit")("sk_682083b708dd971ac5ef22661babf1cd");
 
 const DB = require("../data/helpers");
 const router = express.Router();
 
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
 // POST request
 router.post("/", (req, res) => {
   const id = req.body.visitor_id;
-  const request = req.body // create request from request body
-  console.log(request)
+  const email = req.body.email; // create request from request body
+
   DB.findVisitorById(id)
     .then(visitor => {
       if (visitor.requests_count < 5) {
-        clearbit.Enrichment.find({email: 'maxime@croissant.io', stream: true})
+        clearbit.Enrichment.find({ email: email, stream: true })
+          .then(enrichment => {
+            // Clearbit has enrich visitor Enrichment
+            res.status(200).json(enrichment);
+          })
           .then(res => {
-            console.log(res)
-          })
-          .catch(function (err) {
-            console.error(err);
-          })
-          .then(request => {
-          DB.updateVisitorRequestsCount(id)
-          .then(success => {
-              // Visitor has been successfully updated!
+            DB.updateVisitorRequestsCount(id);
           })
           .catch(err => {
             res.status(500).json({ message: "Failed to update visitor!" });
           });
-        })
       } else {
         res.send(`You have used your 5 free credits!`);
       }
